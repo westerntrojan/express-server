3da4a702d3861133f4380812ead73ed374673f05
westerntrojan <westerntrojan@gmail.com>
2020-06-15 17:35:45 +0500

    image optimization fixed

--- src/middleware/optimizeImage.ts
+++ src/middleware/optimizeImage.ts
@@ -4,7 +4,11 @@ import {optimizeImage} from '../utils/imagemin';
 
 export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 	if (req.file) {
-		await optimizeImage(`src/uploads/${req.body.userId}/${req.file.filename}`);
+		if (process.env.NODE_ENV === 'production') {
+			await optimizeImage(`public/uploads/${req.body.userId}/${req.file.filename}`);
+		} else {
+			await optimizeImage(`src/uploads/${req.body.userId}/${req.file.filename}`);
+		}
 	}
 
 	next();
